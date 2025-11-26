6f4b63f3bac41c2f444a00b469eeb9f649da4054
Your Name <lucas.silva4219@etec.sp.gov.br>
2025-11-26 09:16:53 -0300

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'model' => $this->faker->words(3, true),
+            'marca' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph,
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'image' => $this->faker->imageUrl(400, 300, 'products', true),
+            'category_id' => \App\Models\Category::factory(),
+        ];
+    }
+}
